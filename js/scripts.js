138006d5b7ec9bf476ea88323d4aaddb5605a1ca
henshammi <henshammi@gmail.com>
2024-08-22 20:50:55 +0700

    init

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Document is ready!");
+  const heading = document.querySelector("h1");
+  heading.style.color = "#0066cc";
+});
