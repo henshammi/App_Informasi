79eef6f0b1a846ce54524033ba61b171908fe081
henshammi <henshammi@gmail.com>
2024-08-27 15:14:01 +0700

    push

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,21 +1,32 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
+document.addEventListener("DOMContentLoaded", function () {
+  // Element selection
+  let logOutBtn = document.querySelector("#log_out");
 
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
-});
+  // Event listener for logout
+  logOutBtn.addEventListener("click", () => {
+    // Clear local storage
+    localStorage.removeItem("userId");
+    localStorage.removeItem("userName");
 
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  menuBtnChange();
-});
+    // Redirect to login page
+    window.location.href = "login.html";
+  });
 
-function menuBtnChange() {
-  if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-  } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+  // Sidebar toggle functionality here
+  let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
+
+  closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+  });
+
+  function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else {
+      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+    }
   }
-}
+});
